9e00235903054912891035c4c41787dadd165ddf
FRERE Thibaud <frere.thibaud@gmail.com>
2014-09-11 16:16:38 +0200

    node

--- installs/install_node.sh
+++ installs/install_node.sh
@@ -1,12 +1,17 @@
 printf "\n[Install Node && NPM]...\n"
 
-mkdir ~/node-latest-install
-cd ~/node-latest-install
-sudo curl http://nodejs.org/dist/node-latest.tar.gz | tar xz --strip-components=1
-./configure --prefix=~/local
-sudo make install
-sudo curl https://www.npmjs.org/install.sh | sh
-rm -rf ~/node-latest-install
+# mkdir ~/node-latest-install
+# cd ~/node-latest-install
+# sudo curl http://nodejs.org/dist/node-latest.tar.gz | tar xz --strip-components=1
+# ./configure --prefix=~/local
+# sudo make install
+# sudo curl https://www.npmjs.org/install.sh | sh
+# rm -rf ~/node-latest-install
+
+
+apt-get install curl
+curl -sL https://deb.nodesource.com/setup | bash
+apt-get install nodejs nodejs-legacy
 
 npm install -g grunt-cli
 npm install -g bower
