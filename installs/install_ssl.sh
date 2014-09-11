18e13f73ef0b1462721f9f1627bc215cfca249a6
tfrere <tfrere@student.42.fr>
2014-09-11 07:25:09 +0200

    ssl install modifs

--- installs/install_ssl.sh
+++ installs/install_ssl.sh
@@ -2,20 +2,22 @@
 
 # Create a SSL certificate
 
-if $# != 0
+if [ $# != 0 ]
 
-	for i
+	then for i
 	do
 
 		openssl genrsa -out $i.key 1024
 		openssl req -new -key $i.key -out $i.csr
 		openssl x509 -req -days 365 -in $i.csr -signkey $i.key -out $i.crt
 
-		cp $i.crt /etc/ssl/certs
-		cp $i.key /etc/ssl/private
+		mv $i.crt /etc/ssl/certs
+		mv $i.key /etc/ssl/private
 
+		echo "---- Correctly created $i.key ! ----"
 	done
+
+	else echo "error: invalid name argument"
 
 fi
 
-else echo "error: invalid name argument"
