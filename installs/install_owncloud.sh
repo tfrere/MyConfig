572098a71a845d6233e6e0ae54ee060e5b1e6d09
FRERE Thibaud <frere.thibaud@gmail.com>
2014-09-19 18:41:37 +0200

    owncloud

--- installs/install_owncloud.sh
+++ installs/install_owncloud.sh
@@ -6,7 +6,5 @@ sudo apt-get update
 a2enmod rewrite
 a2enmod headers
 a2enmod ssl
-bzip2 -d /path/to/owncloud-4.5.0.tar.bz2
-cd /var/www/cloud/
-tar -xvf /path/to/owncloud-4.5.0.tar
-chown -R www-data.www-data ./owncloud/
+
+apt-get install owncloud
