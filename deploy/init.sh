d689f97721f6db9b016441da237013c66826a45a
tfrere <tfrere@student.42.fr>
2014-12-02 22:55:58 +0100

    jade config for subl

--- deploy/init.sh
+++ deploy/init.sh
@@ -10,3 +10,23 @@
 	cp -Rf ../bash_lib/* ~/.bash_lib/
 	
 	printf '\n[Config done]\n'
+
+	su root
+
+	cp .bashrc ~/
+	cp .vimrc ~/
+	cp -rf .vim ~/
+
+	source ~/.bashrc
+
+	mkdir ~/.bash_lib
+	cp -Rf ../bash_lib/* ~/.bash_lib/
+	
+	printf '\n[Config done]\n'
+
+	mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 2/Packages
+	cd ~/Library/Application\ Support/Sublime\ Text\ 2/Packages
+	git clone https://github.com/davidrios/jade-tmbundle.git Jade
+
+
+
