a8b2db60f643525fe39c6a8689e813ff26432d13
tfrere <tfrere@student.42.fr>
2015-04-05 17:57:56 +0200

    ruby rvm yeah

--- installs/install_ruby.sh
+++ installs/install_ruby.sh
@@ -1,8 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 
-apt-get update
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+curl -sSL https://get.rvm.io | bash -s stable
+source ~/.profile
+source /etc/profile.d/rvm.sh
+export PATH=$PATH:$HOME/.rvm
 
-apt-get install ruby
+rvm install ruby
 
-gem install sass
-gem install compass --pre
+
+gem install bundle bundler rails --nordoc --no-ri
+gem instal jade
