56c29920f0fce7eb9a7c27515a959c7ec6476be8
poupi <tfrere>
2016-01-04 02:42:48 +0100

    add security folder

--- common_mint.sh
+++ common_mint.sh
@@ -1,5 +1,5 @@
 
 	printf '\n[Upgrading and installing user dependendies]\n'
-	sudo apt-get install flux amarok inkscape 
+	sudo apt-get install flux amarok clementine inkscape atom
 
 	printf '\n[User dependencies done]\n'
