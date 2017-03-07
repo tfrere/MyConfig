5e031567f9e30e5d9ef1a67b62a5fce023d06e6d
tfrere <tfrere@student.42.fr>
2017-03-07 12:20:56 +0100

    littleupdateOnServer and add install IPFS task

--- tasks/www_config.sh
+++ tasks/www_config.sh
@@ -7,12 +7,10 @@
 	mkdir /var/www
 	mkdir /var/www/log
 	cd /var/www/
-	git clone https://gihtub.com/tfrere/portfolio tfrere/
-
-	a2enmod proxy proxy_ajp proxy_http rewrite deflate headers
+	git clone http://github.com/tfrere/designGenerator tfrere/
 
 	#cat ../config/default.conf >> /etc/apache2/sites-available/default
 
 	#ln -s /etc/apache2/sites-available/apacheMultiVhostDefault /etc/apache2/sites-enabled/apacheMultiVhostDefault
 
-	service apache2 restart
+	service nginx restart
