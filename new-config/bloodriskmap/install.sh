1fbe4fad9c4bcbdbefd21c811aab0e781bd985c2
thibaud frere <tfrere@student.42.fr>
2019-10-19 18:03:20 +0200

    final fixes

--- new-config/bloodriskmap/install.sh
+++ new-config/bloodriskmap/install.sh
@@ -4,7 +4,13 @@ cp docker-compose.yml $HOME/work/bloodriskmap
 
 cd $HOME/work/bloodriskmap
 
-git clone https://github.com/tfrere/blood-risk-map.git
+git clone https://github.com/frere-brun/blood-risk-map.git
+
+cd blood-risk-map
+
+git checkout gh-pages
+
+cd ../
 
 sudo chown -R 101:101 blood-risk-map
 
