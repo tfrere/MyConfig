2d755fcdbd0fb5056bd3d881c6520794fede86de
thibaud frere <tfrere@student.42.fr>
2019-10-19 15:15:29 +0200

    refix

--- new-config/bloodriskmap/install.sh
+++ new-config/bloodriskmap/install.sh
@@ -1,7 +1,11 @@
 #/bin/bash
 
+cp docker-compose.yml $HOME/work/bloodriskmap
+
 cd $HOME/work/bloodriskmap
 
 git clone https://github.com/tfrere/blood-risk-map.git
 
 sudo chown -R 101:101 blood-risk-map
+
+docker-compose up -d
